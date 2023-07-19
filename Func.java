d6d9ab023ebd9f5c9f401771a6c6e5ad443261d8
arbaz5656 <139984287+arbaz5656@users.noreply.github.com>
2023-07-19 23:48:41 +0530

    Add files via upload
    function in java

---
+++ Func.java
@@ -0,0 +1,23 @@
+public class Func {
+//    public static void function(){
+//        System.out.println("hello");
+//    }
+
+      public static void add(int a, int b){
+          int c=a+b;
+          System.out.println(c);
+      }
+
+    public static void main(String[] args){
+//        System.out.println("Hello Arbaz");
+//        System.out.println("Hello Arbaz");
+//        System.out.println("Hello Arbaz");
+//          function();
+//          function();
+//          function();
+          add(4,8);
+
+
+
+    }
+}
