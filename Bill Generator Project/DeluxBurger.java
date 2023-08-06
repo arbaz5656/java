e4cb77577f42229871188a54cfb1ef106a3a15fb
arbaz5656 <139984287+arbaz5656@users.noreply.github.com>
2023-08-07 00:39:13 +0530

    Delux file extra

---
+++ Bill Generator Project/DeluxBurger.java
@@ -0,0 +1,16 @@
+//class DeluxBurger extends Burger{
+//
+//    public DeluxBurger(boolean veg) {
+//        super(veg);
+//        super.Addcoca_cola();
+//        super.Addfrench_fries();
+//        super.Addsofty();
+//    }
+//
+//    public void Addcoca_cola(){
+//    }
+//    public void  Addfrench_fries(){
+//    }
+//    public void  Addsofty(){
+//    }
+//}
